c26244fff9d584b1883008a0243da22e81fb4816
minman79 <minman79@protonmail.com>
2024-10-08 13:04:13 +0100

    Finished off the mintDsc function with an error in the event mint fails

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -55,6 +55,7 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__NotAllowedToken();
     error DSCEngine__TransferFailed();
     error DSCEngine__BreaksHealthFactor(uint256 healthFactor);
+    error DSCEngine__MintFailed();
 
     //////////////////////
     // State Variables  //
@@ -152,6 +153,10 @@ contract DSCEngine is ReentrancyGuard {
         s_DSCMinted[msg.sender] += amountDscToMint;
         // If they minted too much ($150 DSC dept to $100 ETH collateral)
         _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if (!minted) {
+            revert DSCEngine__MintFailed();
+        }
     }
 
     function burnDsc() external {}
